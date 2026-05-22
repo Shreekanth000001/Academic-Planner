34ef66b6b9c9b7ae8744596e5d0cf827c67dfc6d
Shreekanth.k <Shreekanth.k000001@gmail.com>
2026-05-22 19:11:59 +0530

    Installed clerk for authentication and updated frontend to be wrapped by cleark wrapper

---
+++ src/proxy.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+// This automatically handles the authentication state
+export default clerkMiddleware()
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for Clerk's auto-proxy path (CRITICAL NEW RULE)
+    '/__clerk/(.*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
